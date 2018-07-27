7263dce3e64a0e49018e189cfc7008265bfd2c18
Mateusz Żółtak <mateusz.zoltak@oeaw.ac.at>
2018-07-27 11:44:15 +0200

    CORS headers added

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ use acdhOeaw\resolver\Resolver;
 require_once __DIR__ . '/vendor/autoload.php';
 $cl = new ClassLoader();
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+
 RC::init('config.ini');
 Resolver::$debug = filter_input(\INPUT_GET, 'debug') ? true : false;
 
